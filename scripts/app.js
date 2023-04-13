058606b9f12591abcba7373a1854b8daafd8f5ab
Ghost116rus <danilkaleev@mai.ru>
2023-04-13 23:06:43 +0300

    Добавил lazy-load на основную страницу. Практически закончил писать страницу админа

--- scripts/app.js
+++ scripts/app.js
@@ -1,77 +1,103 @@
+
+
 var main = function () {
     "use strict";
 
-    var toDos = [
-        "Закончить писать эту книгу",
-        "Вывести Грейси на прогулку в парк",
-        "Ответить на электронные письма",
-        "Подготовиться к лекции в понедельник",
-        "Обновить несколько новых задач",
-        "Купить продукты"
+    class Medicine{
+        description;
+        imageSrc;
+        constructor(description, imageSrc){
+            this.description = description;
+            this.imageSrc = imageSrc;
+        }
+    }
+
+    var Special = [
+        new Medicine("СофтЛайф премиум ватные палочки на бумажной основе с добавлением бамбука №100","../img/medecine/1.jpg"),
+        new Medicine("СофтЛайф премиум ватные диски прямоугольные черные №80","../img/medecine/2.jpg"),
+        new Medicine("Будь Здоров! Рыбный жир 75мл","../img/medecine/3.jpg"),
+        new Medicine("Будь Здоров! Грин фиточай Фитосбор №1 толокнянка ф/п 2г №20","../img/medecine/4.jpg"),
+        new Medicine("АВС хэлси фуд клетчатка Сибирская стопаппетит 8,5г №14","../img/medecine/5.jpg")
     ];
 
-    var toDDos =
+    var Medicines =
         [
-            "Купить продукты",
-            "Обновить несколько новых задач",
-            "Подготовиться к лекции в понедельник",
-            "Ответить на электронные письма",
-            "Вывести Грейси на прогулку в парк",
-            "Закончить писать эту книгу"
+            new Medicine("СофтЛайф премиум ватные палочки на бумажной основе с добавлением бамбука №100","../img/medecine/1.jpg"),
+            new Medicine("СофтЛайф премиум ватные диски прямоугольные черные №80","../img/medecine/2.jpg"),
+            new Medicine("Будь Здоров! Рыбный жир 75мл","../img/medecine/3.jpg"),
+            new Medicine("Будь Здоров! Грин фиточай Фитосбор №1 толокнянка ф/п 2г №20","../img/medecine/4.jpg"),
+            new Medicine("АВС хэлси фуд клетчатка Сибирская стопаппетит 8,5г №14","../img/medecine/5.jpg"),
+            new Medicine("Инвитро Здоровье щитовидной железы","https://cdn.budzdorov.ru/media/catalog/product/cache/1e8ef93b9b4867ab9f3538dde2cb3b8a/1/1/113044-c-0-8-9-c089a3e77f904efd2884c5ab3cdad57d3b9ebc0b_113044.jpg"),
+            new Medicine("СофтЛайф премиум ватные палочки на бумажной основе с добавлением бамбука №100","../img/medecine/1.jpg"),
+            new Medicine("СофтЛайф премиум ватные диски прямоугольные черные №80","../img/medecine/2.jpg"),
+            new Medicine("Будь Здоров! Рыбный жир 75мл","../img/medecine/3.jpg"),
+            new Medicine("Будь Здоров! Грин фиточай Фитосбор №1 толокнянка ф/п 2г №20","../img/medecine/4.jpg"),
+            new Medicine("АВС хэлси фуд клетчатка Сибирская стопаппетит 8,5г №14","../img/medecine/5.jpg"),
+            new Medicine("Инвитро Здоровье щитовидной железы","https://cdn.budzdorov.ru/media/catalog/product/cache/1e8ef93b9b4867ab9f3538dde2cb3b8a/1/1/113044-c-0-8-9-c089a3e77f904efd2884c5ab3cdad57d3b9ebc0b_113044.jpg"),
+            new Medicine("СофтЛайф премиум ватные палочки на бумажной основе с добавлением бамбука №100","../img/medecine/1.jpg"),
+            new Medicine("СофтЛайф премиум ватные диски прямоугольные черные №80","../img/medecine/2.jpg"),
+            new Medicine("Будь Здоров! Рыбный жир 75мл","../img/medecine/3.jpg"),
+            new Medicine("Будь Здоров! Грин фиточай Фитосбор №1 толокнянка ф/п 2г №20","../img/medecine/4.jpg"),
+            new Medicine("АВС хэлси фуд клетчатка Сибирская стопаппетит 8,5г №14","../img/medecine/5.jpg"),
+            new Medicine("Инвитро Здоровье щитовидной железы","https://cdn.budzdorov.ru/media/catalog/product/cache/1e8ef93b9b4867ab9f3538dde2cb3b8a/1/1/113044-c-0-8-9-c089a3e77f904efd2884c5ab3cdad57d3b9ebc0b_113044.jpg"),
+            new Medicine("СофтЛайф премиум ватные палочки на бумажной основе с добавлением бамбука №100","../img/medecine/1.jpg"),
+            new Medicine("СофтЛайф премиум ватные диски прямоугольные черные №80","../img/medecine/2.jpg"),
+            new Medicine("Будь Здоров! Рыбный жир 75мл","../img/medecine/3.jpg"),
+            new Medicine("Будь Здоров! Грин фиточай Фитосбор №1 толокнянка ф/п 2г №20","../img/medecine/4.jpg"),
+            new Medicine("АВС хэлси фуд клетчатка Сибирская стопаппетит 8,5г №14","../img/medecine/5.jpg"),
+            new Medicine("Инвитро Здоровье щитовидной железы","https://cdn.budzdorov.ru/media/catalog/product/cache/1e8ef93b9b4867ab9f3538dde2cb3b8a/1/1/113044-c-0-8-9-c089a3e77f904efd2884c5ab3cdad57d3b9ebc0b_113044.jpg"),
         ];
 
-    var makeContent = function(Container) {
+    var makeContent = function(Container, medic) {
         var offers = $('<div/>', {
             class:  'offers',
         }).appendTo(Container);
 
-        var offer = $('<div/>', {
-            class:  'offer',
-        }).appendTo(offers);
-        var image = $('<a/>').attr("href", "DeliveryPage.html").appendTo(offer);
-        $('<img>').attr("src","../img/medecine/1.jpg").appendTo(image);
-        $('<a/>', {
-            href: "DeliveryPage.html",
-            text: "СофтЛайф WWW премиум ватные палочки на бумажной основе с добавлением бамбука №100"
-        }).appendTo(offer);
+        medic.forEach(function (medicine) {
+            var offer = $('<div/>', {
+                class:  'offer',
+            }).appendTo(offers);
+            var image = $('<a/>').attr("href", "DeliveryPage.html").appendTo(offer);
+            var medicineImage = $('<img>', {
+                src: medicine.imageSrc,
+                class: 'lazy',
+            });
+            medicineImage.attr("data-src", medicine.imageSrc);
+            medicineImage.appendTo(image);
+            $('<a/>', {
+                href: "DeliveryPage.html",
+                text: medicine.description
+            }).appendTo(offer);
+            console.log(medicine.imageSrc)
+        });
     }
 
     $(".tabs a span").toArray().forEach(function (element) {
         //создаем обработку щелчков для этого элемента
-        $(element).on("click", function () {
+        $(element).on("click", function ()
+        {
             var $element = $(element),
                 $content;
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
             if ($element.parent().is(":nth-child(1)")) {
-                $content = $("<ul>");
-                for (let i = 1; i <= toDos.length; i++)
-                {
-                    $content.append($("<li>").text(toDos[toDos.length - i]));
-                }
-                $("main .content").append($content);
+                $content = $("<div>");
+                makeContent($content, Special);
             } else if ($element.parent().is(":nth-child(2)")) {
-                $content = $("<ul>");
-                toDos.forEach(function (todo) {
-                    $content.append($("<li>").text(todo));
-                });
+                $content = $("<div>");
+                makeContent($content, Medicines);
 
             } else if ($element.parent().is(":nth-child(3)")) {
                 $content = $("<div>");
-                makeContent($content);
-
-
             }
             $("main .content").append($content);
+
         })
-            return false;
+        return false;
         });
     $(".tabs a:first-child span").trigger("click");
 
 };
 
 $(document).ready(main);
-
-// <a href="#"><img src="../img/medecine/1.jpg"></a>
-// <a href="#">СофтЛайф премиум ватные палочки на бумажной основе с добавлением бамбука №100</a>
---
+++ scripts/mainPage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("img.lazy").Lazy({
+        threshold: 0,
+        effect: 'fadeIn',
+        effectTime: 500
+    });
+});
